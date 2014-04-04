e067eadab95062c264c297a7ae9f2c017e3fb98d
Ivana Setiawan <ivana@mangrove.nl>
2014-04-04 16:24:22 +0200

    Add JS

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,18 @@
 /* Runs when the DOM is loaded. */
 $(document).ready(function() {
+  // Smooth scroll
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
