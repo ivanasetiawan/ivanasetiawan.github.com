b8752f721e1b9354d1d3651616dae84de9368077
Ivana Setiawan <ivana@funda.nl>
2016-01-04 20:02:11 +0100

    Update config

--- config.rb
+++ config.rb
@@ -88,7 +88,6 @@ require_relative "./lib/build_cleaner"
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash
   activate :relative_assets
   activate :build_cleaner
 end
