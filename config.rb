2dc2d383f5e4170a15a3dd4e1fd9cf37bfd3aa05
Ivana Setiawan <ivana@funda.nl>
2016-01-04 19:32:36 +0100

    MM started

--- config.rb
+++ config.rb
@@ -1,27 +1,81 @@
-# Require any additional compass plugins here.
+###
+# Compass
+###
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-# Path for images
-http_images_path = "/assets/images/"
-http_generated_images_path = "/assets/images/"
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
-css_dir = "assets/stylesheets"
-sass_dir = "assets/stylesheets/sass"
-images_dir = "assets/images"
-javascripts_dir = "assets/js"
+###
+# Page options, layouts, aliases and proxies
+###
 
-output_style = :compressed
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+###
+# Helpers
+###
 
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+activate :blog do |blog|
+  blog.permalink = "blog/{year}/{title}"
+  blog.paginate = true
+  blog.page_link = "p{num}"
+  blog.per_page = 1
+end
+
+activate :directory_indexes
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
