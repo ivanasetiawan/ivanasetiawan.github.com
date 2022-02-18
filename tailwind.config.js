6fe29a7346a723c9fc686701783dba25bba507bf
Ivana Setiawan <ivana.setiawan@gmail.com>
2022-02-18 22:21:34 +0100

    feat: Homepage, microdata next

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
     './_layouts/**/*.html',
     './_posts/*.md',
     './*.html',
+    './*.md',
   ],
   darkMode: false,
   theme: {
+    fontFamily: {
+      tpr: "tpr, open sans, sans-serif",
+      monospace: "monospace, 'Courier'"
+    },
     extend: {},
   },
   variants: {},
